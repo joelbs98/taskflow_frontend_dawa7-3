5475ec0ee975b77256d9827acd9a432b2612f11b
joelbs98 <joelbarbasa98@gmail.com>
2026-04-08 17:03:50 -0500

    Nuestro primer componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('taskflow-frontend');
+  titulo = 'Taskflow';
+  descripcion = 'Sistema web de gestión de tareas';
+  curso = 'Desarrollo de aplicaciones web Avanzado';
 }
